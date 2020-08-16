807595d3b81c064e798a9e91d85b3844ffc52a5e
pc1423 <pc1423@hanmail.net>
2020-08-16 10:26:00 +0900

    계산기 함수에 대한 설명 주석 추가

--- app/src/main/java/com/example/calc/ButtonInterface.java
+++ app/src/main/java/com/example/calc/ButtonInterface.java
@@ -28,16 +28,29 @@ public class ButtonInterface {
         }
     }
 
+    /**
+     * 숫자 버튼 이외의 버튼이 눌렸을때 호출한다.
+     *
+     * @param type : 사칙연산과 계산버튼에 대한 타입
+     */
     public void onButton(Type type) {
         if (stack.size() != 0 && !Type.contains(stack.lastElement())) {
             stack.add(type.typeString);
         }
     }
 
+    /**
+     * 숫자버튼이 눌렸을때 호출한다.
+     * @param number : 정수형 숫자
+     */
     public void onButton(int number) {
         stack.add(String.valueOf(number));
     }
 
+    /**
+     * 버튼이 하나 눌렸을때 마다 호출하여 textView에 수식으로 표시한다.
+     * @return : 현재 완성된 수식
+     */
     public String getExpression() {
         return stack.toString().replace("[", "").replace("]", "").replace(",", "").replace(" ", "");
     }
